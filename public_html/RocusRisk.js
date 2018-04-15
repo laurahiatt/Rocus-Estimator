7bfc57f5a7721db019b8aa74e75719d58d6fb06e
laurahiatt <lahiatt@davidson.edu>
2018-04-15 12:10:49 -0400

    Have some d3 code working

--- public_html/RocusRisk.js
+++ public_html/RocusRisk.js
@@ -34,26 +34,108 @@ function clickHandle() {
 	document.getElementById("costWithout").value = costWO;
 }
 
-var numbers=[40, 130, 75, 170];
-var svg = d3.select("svg");
+function radialProgress(selector) {
+  const parent = d3.select(selector)
+  const size = parent.node().getBoundingClientRect()
+  const svg = parent.append('svg')
+    .attr('width', size.width)
+    .attr('height', size.height);
+  const outerRadius = Math.min(size.width, size.height) * 0.45;
+  const thickness = 10;
+  let value = 0;
+  
+  const mainArc = d3.arc()
+    .startAngle(0)
+    .endAngle(Math.PI * 2)
+    .innerRadius(outerRadius-thickness)
+    .outerRadius(outerRadius)
 
-var selection = svg.selectAll("g")
-    .data(numbers)
-    .enter().append("g")
-    .attr("transform", (d,i) => {
-        return "translate(" + 40*i + "," + {200-d} + ")"; });
+  svg.append("path")
+    .attr('class', 'progress-bar-bg')
+    .attr('transform', `translate(${size.width/2},${size.height/2})`)
+    .attr('d', mainArc())
+  
+  const mainArcPath = svg.append("path")
+    .attr('class', 'progress-bar')
+    .attr('transform', `translate(${size.width/2},${size.height/2})`)
+  
+  svg.append("circle")
+    .attr('class', 'progress-bar')
+    .attr('transform', `translate(${size.width/2},${size.height/2-outerRadius+thickness/2})`)
+    .attr('width', thickness)
+    .attr('height', thickness)
+    .attr('r', thickness/2)
 
-selection.append("rect")
-    .attr("width", 39)
-    .attr("height", (d,i) => {return d; });
+  const end = svg.append("circle")
+    .attr('class', 'progress-bar')
+    .attr('transform', `translate(${size.width/2},${size.height/2-outerRadius+thickness/2})`)
+    .attr('width', thickness)
+    .attr('height', thickness)
+    .attr('r', thickness/2)
+  
+  let percentLabel = svg.append("text")
+    .attr('class', 'progress-label')
+    .attr('transform', `translate(${size.width/2},${size.height/2})`)
+    .text('0')
 
-selection.append("text")
-    .attr("x", 25)
-    .attr("y", 25)
-    .text((d) => {return d/10; });
+  return {
+    update: function(progressPercent) {
+      const startValue = value
+      const startAngle = Math.PI * startValue / 50
+      const angleDiff = Math.PI * progressPercent / 50 - startAngle;
+      const startAngleDeg = startAngle / Math.PI * 180
+      const angleDiffDeg = angleDiff / Math.PI * 180
+      const transitionDuration = 1500
 
+      mainArcPath.transition().duration(transitionDuration).attrTween('d', function(){
+        return function(t) {
+          mainArc.endAngle(startAngle + angleDiff * t)
+          return mainArc();
+        }
+      })
+      end.transition().duration(transitionDuration).attrTween('transform', function(){
+        return function(t) {
+          return `translate(${size.width/2},${size.height/2})`+
+            `rotate(${(startAngleDeg + angleDiffDeg * t)})`+
+            `translate(0,-${outerRadius-thickness/2})`
+        }
+      })
+      percentLabel.transition().duration(transitionDuration).tween('bla', function() {
+        return function(t) {
+          percentLabel.text(Math.round(startValue + (progressPercent - startValue) * t));
+        }
+      })
+      value = progressPercent
+    }
+  }
+}
 
 window.onload=function(){
 	var button = document.getElementById("calc");
 	button.addEventListener("click",clickHandle);
+        var numbers=[40, 130, 75, 170];
+        var svg = d3.select("svg");
+
+        var selection = svg.selectAll("g")
+            .data(numbers)
+            .enter().append("g")
+            .attr("transform", (d,i) => {
+                return "translate(" + 40*i + "," + (200-d) + ")"; });
+
+        selection.append("rect")
+            .attr("width", 39)
+            .attr("height", (d,i) => {return d; });
+
+        selection.append("text")
+            .attr("x", 25)
+            .attr("y", 25)
+            .text((d) => {return d/10; });
+
+        let chart = radialProgress('.widget')
+        let progress = [100,0,5,20,35,70,90,100,0]
+        let state = 0
+        d3.interval(function(){
+          chart.update(progress[state])
+          state = (state + 1) % progress.length
+        }, 2000)
 }
