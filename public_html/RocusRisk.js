dacdd92d880ab51ffb5f1176adb74eb0fbd6412f
laurahiatt <lahiatt@davidson.edu>
2018-04-20 00:04:42 -0400

    Attempting to add bar

--- public_html/RocusRisk.js
+++ public_html/RocusRisk.js
@@ -62,6 +62,7 @@ function radialProgress(selector) {
   
   const mainArcPath = svg.append("path")
     .attr('class', 'progress-bar')
+    .attr('fill', "blue")
     .attr('transform', `translate(${size.width/2},${size.height/2})`);
   
   svg.append("circle")
@@ -75,7 +76,7 @@ function radialProgress(selector) {
     .attr('class', 'progress-bar')
     .attr('transform', `translate(${size.width/2},${size.height/2-outerRadius+thickness/2})`)
     .attr('width', thickness)
-    .attr('height', thickness)
+    .attr('height', thickness) 
     .attr('r', thickness/2);
   
   let percentLabel = svg.append("text")
@@ -120,24 +121,6 @@ window.onload=function(){
 	var button = document.getElementById("calc");
         button.addEventListener("click",clickHandle);
         button.addEventListener("click", function() {
-            var numbers=[40, 130, 75, 170];
-            var svg = d3.select("svg");
-
-            var selection = svg.selectAll("g")
-                .data(numbers)
-                .enter().append("g")
-                .attr("transform", (d,i) => {
-                    return "translate(" + 40*i + "," + (200-d) + ")"; });
-
-            selection.append("rect")
-                .attr("width", 39)
-                .attr("height", (d,i) => {return d; });
-
-            selection.append("text")
-                .attr("x", 25)
-                .attr("y", 25)
-                .text((d) => {return d/10; });
-
             chart = radialProgress('.widget');
             var p = document.getElementById("prob");
             var pval = p.value * 100;
@@ -150,3 +133,96 @@ window.onload=function(){
         });
         
 };
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1, 1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("data.tsv", function(error, data) {
+
+  data.forEach(function(d) {
+    d.frequency = +d.frequency;
+  });
+
+  x.domain(data.map(function(d) { return d.letter; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
+
+  d3.select("input").on("change", change);
+
+  var sortTimeout = setTimeout(function() {
+    d3.select("input").property("checked", true).each(change);
+  }, 2000);
+
+  function change() {
+    clearTimeout(sortTimeout);
+
+    // Copy-on-write since tweens are evaluated after a delay.
+    var x0 = x.domain(data.sort(this.checked
+        ? function(a, b) { return b.frequency - a.frequency; }
+        : function(a, b) { return d3.ascending(a.letter, b.letter); })
+        .map(function(d) { return d.letter; }))
+        .copy();
+
+    svg.selectAll(".bar")
+        .sort(function(a, b) { return x0(a.letter) - x0(b.letter); });
+
+    var transition = svg.transition().duration(750),
+        delay = function(d, i) { return i * 50; };
+
+    transition.selectAll(".bar")
+        .delay(delay)
+        .attr("x", function(d) { return x0(d.letter); });
+
+    transition.select(".x.axis")
+        .call(xAxis)
+      .selectAll("g")
+        .delay(delay);
+  }
+});
